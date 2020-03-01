711731de6102167d073c5354f9029480bad18515
christopher.duroseau <christopher.duroseau@christopherduroseaus-MacBook-Pro.local>
2020-03-01 15:39:03 -0500

    fixed scrolling issue

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // Responsive Nav
-$(function() {
+$(function () {
   menu = $('nav ul');
 
-  $('#openup').on('click', function(e) {
+  $('#openup').on('click', function (e) {
     e.preventDefault();
     menu.slideToggle();
   });
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     var w = $(this).width();
     if (w > 480 && menu.is(':hidden')) {
       menu.removeAttr('style');
     }
   });
 
-  $('nav li').on('click', function(e) {
+  $('nav li').on('click', function (e) {
     var w = $(window).width();
     if (w < 480) {
       menu.slideToggle();
@@ -24,20 +24,16 @@ $(function() {
 });
 
 // Smooth Scrolling
-$('.cf a').on('click', function(event) {
+$('.cf a').on('click', function (event) {
   if (this.hash !== '') {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - 70
       },
-      800,
-      function() {
-        window.location.hash = hash;
-      }
+      800
     );
   }
 });
